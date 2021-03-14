d1e2b1f2754bcc0122a0e049b7a5b059dbbc8066
hungphongbk <programmingd32@gmail.com>
2021-03-14 14:25:25 +0700

    complete backend

--- backend/src/controllers/RecordController.ts
+++ backend/src/controllers/RecordController.ts
@@ -2,7 +2,8 @@ import { Controller, Get, QueryParams } from "@tsed/common";
 import { Inject } from "@tsed/di";
 import { RecordRepository } from "../repositories/RecordRepository";
 import { RecordMetricType } from "../entities/Record";
-
+import { addDays, format, differenceInDays } from "date-fns";
+//
 @Controller("/data")
 export class RecordController {
   @Inject()
@@ -12,9 +13,20 @@ export class RecordController {
   async get(
     @QueryParams("start_date") startDate: Date,
     @QueryParams("end_date") endDate: Date,
-    @QueryParams("metrics") metrics: RecordMetricType
+    @QueryParams("metrics") metrics: string
   ) {
-    const data = (await this.repo.find({ metrics })).map((r) => r.data);
+    const count = differenceInDays(endDate, startDate);
+    const data = (
+        await this.repo.find({
+          metrics: (metrics as unknown) as RecordMetricType,
+        })
+      )
+        .slice(count)
+        .map((r) => r.data),
+      labels = [];
+    for (let i = 0; i < count; i++) {
+      labels.push(addDays(startDate, i));
+    }
     return {
       success: true,
       data: {
@@ -22,15 +34,15 @@ export class RecordController {
           {
             date_ranges: [
               {
-                start_date: startDate,
-                end_date: endDate,
+                start_date: format(startDate, "yyyy-MM-dd"),
+                end_date: format(endDate, "yyyy-MM-dd"),
               },
             ],
             metrics: metrics,
             labels: [],
             datasets: [
               {
-                label: "",
+                label: labels.map((d) => format(d, "yyyy-MM-dd")),
                 data,
               },
             ],
